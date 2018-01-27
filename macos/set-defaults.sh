9ebb7fef6b04a6cb6831e64cdac902c46a8625e3
Tuomo Syvänperä <tuomo.syvanpera@houston-inc.com>
2018-01-27 18:50:26 +0200

    Fix iTerm2 profile

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -84,7 +84,7 @@ defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 ###############################################################################
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 

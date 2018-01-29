9988830d07987148659f2a4b093f9cf8c76b2d3e
Tuomo Syvänperä <tuomo.syvanpera@houston-inc.com>
2018-01-29 14:41:26 +0200

    Tweak macOS settings

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -67,8 +67,6 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 # Finder                                                                      #
 ###############################################################################
 
-# show all filename extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 # show path bar
 defaults write com.apple.finder ShowPathbar -bool true
 # allow text selection in Quick Look

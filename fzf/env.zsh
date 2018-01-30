efe0458ddcbde0ddb9b2f3540936821ffec54bae
Tuomo Syvänperä <tuomo.syvanpera@houston-inc.com>
2018-01-30 08:06:47 +0200

    Clean up

--- fzf/env.zsh
+++
@@ -1,3 +0,0 @@
-FZF_DEFAULT_OPTS='--height 40% --reverse --border'
-
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
--- powerlevel9k/env.zsh
+++
@@ -1,3 +0,0 @@
-POWERLEVEL9K_MODE='nerdfont-complete'
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=( status root_indicator background_jobs )
-source ~/.powerlevel9k/powerlevel9k.zsh-theme

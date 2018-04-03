6f88888edfbb2cfd0e9e14dc9ff94696ac55b525
Tuomo Syvänperä <tuomo.syvanpera@houston-inc.com>
2018-04-03 16:35:47 +0300

    Add Yasnippet

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -353,6 +353,11 @@
   :ensure t
   :after magit)
 
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode t))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
